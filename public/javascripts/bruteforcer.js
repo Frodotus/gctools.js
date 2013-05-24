9be67899b1c6b8335cf2731e5e39a0a2ff5a83c0
Toni Leino <toni@leino.net>
2013-05-24 13:59:48 +0300

    Marker color toggle

--- public/javascripts/bruteforcer.js
+++ public/javascripts/bruteforcer.js
@@ -19,8 +19,13 @@ function allPossibleCases(arr) {
 
 function toggle(button) {
     console.log("Toggle: "+button)
-    document.getElementById(button).setAttribute("class","btn btn-danger");
-    markers[button].setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
+    if(document.getElementById(button).getAttribute("class") == "btn btn-danger") {
+        document.getElementById(button).setAttribute("class","btn");
+        markers[button].setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
+    } else {
+        document.getElementById(button).setAttribute("class","btn btn-danger");
+        markers[button].setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
+    }
 }
 
 function addToggler(marker, button) {
