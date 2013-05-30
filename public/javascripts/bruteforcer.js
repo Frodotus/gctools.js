529a7137b15b931231ff51dcff1631a309164cdf
Toni Leino <toni@leino.net>
2013-05-30 15:35:48 +0300

    Circles for brute map

--- public/javascripts/bruteforcer.js
+++ public/javascripts/bruteforcer.js
@@ -126,9 +126,9 @@ function fetchCaches(){
                 radius: 161,
                 strokeColor: "#FF0000",
                 strokeOpacity: 0.8,
-                strokeWeight: 2,
+                strokeWeight: 1,
                 fillColor: "#FF0000",
-                fillOpacity: 0.35,
+                fillOpacity: 0.05,
                 map: map
              });
          }
